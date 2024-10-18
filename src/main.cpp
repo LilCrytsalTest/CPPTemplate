3451777056deec93b3576854f4025b211feb2f5e
LilCrystal <lil_crystal@163.com>
2024-10-18 20:31:17 +0800

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,10 @@
-#include <boost/system/error_code.hpp>
 #include <iostream>
+#include "TestTool.h"
 
-#include "Utils/TestTool.h"
-
-int main() {
-    std::string text ="Hello, world! This is a test string with numbers 123 and 456.";
-
-    std::cout << std::boolalpha << TestTool::isNumeric(text) << std::endl;
-
-    boost::system::error_code ec;
-    std::cout << "Error code value: " << ec.value() << std::endl;
-    std::cout << "Error message: " << ec.message() << std::endl;
-
-    // 模拟设置一个错误码
-    ec = boost::system::errc::make_error_code(boost::system::errc::invalid_argument);
-    std::cout << "After setting error code value: " << ec.value() << std::endl;
-    std::cout << "After setting error message: " << ec.message() << std::endl;
-
+int main()
+{
+    //For test boost
+    std::string str = "123";
+    std::cout<< std::boolalpha << TestTool::isNumeric(str) << std::endl;
     return 0;
 }
---
+++ test/testMain.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "TestTool.h"
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(TestToolTest, isNumeric_Case1)
+{
+    auto str = "Test";
+    auto result = TestTool::isNumeric(str);
+    ASSERT_EQ(result, false);
+}
+
+TEST(TestToolTest, isNumeric_Case2)
+{
+    auto str = "123456";
+    auto result = TestTool::isNumeric(str);
+    ASSERT_EQ(result, true);
+}
